ff4560c0852e1911cec5e11aad8edbbadf21bb4d
Stephen Montague <Stephen.D.Montague@gmail.com>
2022-04-09 22:17:26 -0400

    Laying out what the project is going to look like.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
 	return (
 		<div className='App'>
 			<h1>Let's Build the TINDER Clone App 🚀</h1>
+
+			{/* Header */}
+			{/* Tinder Cards */}
+			{/* Buttons below cards */}
+
+			{/* Chats screen */}
+			{/* Individual chat screen */}
 		</div>
 	);
 }
